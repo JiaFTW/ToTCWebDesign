8bcf845bde431efff488f657c2d53e696d4b20f4
Ubuntu <ubuntu@ip-172-26-9-19.ec2.internal>
2025-03-30 03:29:00 +0000

    Change the cart_icon.png, few changes in cart.php, cart.css

--- frontend/cart.php
+++ frontend/cart.php
@@ -17,6 +17,7 @@ $cart = isset($_SESSION['cart']) ? $_SESSION['cart'] : [];
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,900;1,9..40,900&family=Faculty+Glyphic&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
   </head>
   <body>
 	<div class="navbar">
@@ -80,7 +81,7 @@ $cart = isset($_SESSION['cart']) ? $_SESSION['cart'] : [];
                 <button class="checkout-btn">Proceed to Checkout</button>
             </div>
         <?php endif; ?>
-        <form action="clear_cart.php" method="POST">
+        <form action="backend/api/clear_cart.php" method="POST">
             <button type="submit">Clear Cart</button>
         </form>
     </div>
