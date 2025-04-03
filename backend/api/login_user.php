9b3524623bbe4720d6a8beb0f9047e204041b34f
Ubuntu <ubuntu@ip-172-26-9-19.ec2.internal>
2025-04-03 03:13:49 +0000

    added login pages for frontned and backend with rabbitmq connection

---
+++ backend/api/login_user.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+require_once __DIR__ . '/../../vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$connection = new AMQPStreamConnection('98.82.149.231', 5672, 'totc', 'Totc2025');
+$channel = $connection->channel();
+$channel->queue_declare('user_requests', false, true, false, false);
+
+$data = json_encode([
+    "action" => "login",
+    "username" => $email,
+    "password" => $password
+]);
+
+$msg = new AMQPMessage($data, ['delivery_mode' => 2]);
+$channel->basic_publish($msg, '', 'user_requests');
+
+$channel->close();
+$connection->close();
+
+// Simulate successful login until RabbitMQ consumer response is implemented
+$_SESSION['username'] = $email;
+header("Location: ../../frontend/index.html");
+exit;
+?>
